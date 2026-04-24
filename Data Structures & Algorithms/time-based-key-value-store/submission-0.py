e0ef21c7323d1265218612d32997ee54272356da
Alya-chan <116194134+shanks28@users.noreply.github.com>
2026-04-24 23:16:19 +0530

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.py
@@ -0,0 +1,51 @@
+# class TimeMap:
+
+#     def __init__(self):
+# 		self.store={}
+		        
+
+#     def set(self, key: str, value: str, timestamp: int) -> None:
+#         if key in self.store:
+# 			self.store[key]=[]
+# 		self.store[key].append([value,timestamp])
+
+#     def get(self, key: str, timestamp: int) -> str:
+# 		res=""
+# 		values=self.store.get(key,[])
+# 		left=0
+# 		right=len(values)-1
+# 		while left <= right{
+# 			mid=(left+right)//2
+# 			if values[mid][1] <= timestamp:
+# 				res=values[mid][0]
+# 				left=mid+1
+# 			else:
+# 				right=mid-1
+# 		}
+# 		return res
+        
+class TimeMap:
+
+    def __init__(self):
+        self.store = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.store:
+            self.store[key] = []
+        self.store[key].append([value, timestamp])
+
+    def get(self, key: str, timestamp: int) -> str:
+        res = ""
+        values = self.store.get(key, [])
+        left = 0
+        right = len(values) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if values[mid][1] <= timestamp:
+                res = values[mid][0]
+                left = mid + 1   # <-- fixed variable name
+            else:
+                right = mid - 1
+
+        return res
