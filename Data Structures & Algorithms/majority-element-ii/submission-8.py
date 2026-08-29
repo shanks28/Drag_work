9988bce61a3a1de039659cd5a978d4e40549be2c
Alya-chan <116194134+shanks28@users.noreply.github.com>
2026-08-30 00:45:42 +0530

    Add: majority-element-ii - submission-8

---
+++ Data Structures & Algorithms/majority-element-ii/submission-8.py
@@ -0,0 +1,11 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        req=len(nums)//3
+        counts={} # value:freq
+        counts["res"]=[]
+        for index,value in enumerate(nums):
+            counts[value]=counts.get(value,0) + 1
+            if counts[value] > req and value not in counts["res"]:
+                counts["res"].append(value)
+        return counts["res"]
+            
